01f670d6997a704eb598e151e8386dd398598830
Fedor Indutny <fedor@indutny.com>
2018-07-18 19:06:52 -0400

    lib: typescript definitions

---
+++ lib/pow.d.ts
@@ -0,0 +1,19 @@
+import { Buffer } from 'buffer';
+
+export class Solver {
+  public solve(complexity: number, prefix?: Buffer);
+}
+
+export interface IVerifierOptions {
+  readonly size: number;
+  readonly n: number;
+  readonly complexity: number;
+  readonly validity?: number;
+  readonly prefix?: Buffer;
+}
+
+export class Verifier {
+  constructor(options: IVerifierOptions);
+
+  public check(nonce: Buffer, complexity?: number);
+}
